147566844cb5d3559a2a66058cda34c7bf9caa47
ricar <ricar@MSI>
2019-08-21 22:11:43 -0500

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace DevelopEstimated
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bt1 = new System.Windows.Forms.Button();
+            this.txtMejor = new System.Windows.Forms.TextBox();
+            this.txtPeor = new System.Windows.Forms.TextBox();
+            this.txtMp = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtTotal = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // bt1
+            // 
+            this.bt1.Location = new System.Drawing.Point(24, 240);
+            this.bt1.Name = "bt1";
+            this.bt1.Size = new System.Drawing.Size(75, 23);
+            this.bt1.TabIndex = 0;
+            this.bt1.Text = "Calculated Time";
+            this.bt1.UseVisualStyleBackColor = true;
+            this.bt1.Click += new System.EventHandler(this.Bt1_Click);
+            // 
+            // txtMejor
+            // 
+            this.txtMejor.Location = new System.Drawing.Point(26, 68);
+            this.txtMejor.Name = "txtMejor";
+            this.txtMejor.Size = new System.Drawing.Size(100, 22);
+            this.txtMejor.TabIndex = 1;
+            // 
+            // txtPeor
+            // 
+            this.txtPeor.Location = new System.Drawing.Point(26, 119);
+            this.txtPeor.Name = "txtPeor";
+            this.txtPeor.Size = new System.Drawing.Size(100, 22);
+            this.txtPeor.TabIndex = 2;
+            // 
+            // txtMp
+            // 
+            this.txtMp.Location = new System.Drawing.Point(26, 184);
+            this.txtMp.Name = "txtMp";
+            this.txtMp.Size = new System.Drawing.Size(100, 22);
+            this.txtMp.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Mejor Tiempo";
+            this.label1.Click += new System.EventHandler(this.Label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Peor Tiempo";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(21, 150);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(145, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Tiempo más probable";
+            // 
+            // txtTotal
+            // 
+            this.txtTotal.Location = new System.Drawing.Point(157, 241);
+            this.txtTotal.Name = "txtTotal";
+            this.txtTotal.Size = new System.Drawing.Size(100, 22);
+            this.txtTotal.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(180, 218);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 17);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Total";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtTotal);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtMp);
+            this.Controls.Add(this.txtPeor);
+            this.Controls.Add(this.txtMejor);
+            this.Controls.Add(this.bt1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bt1;
+        private System.Windows.Forms.TextBox txtMejor;
+        private System.Windows.Forms.TextBox txtPeor;
+        private System.Windows.Forms.TextBox txtMp;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtTotal;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DevelopEstimated
+{
+    public partial class Form1 : Form
+    {
+        /// <summary>
+        /// Variables que se utilizan en la formula para calcular el mejor tiempo
+        ///
+        /// </summary>
+        public int Total = 0;
+        public int Mejor = 0;
+        public int Peor = 0;
+        public int Probable = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Bt1_Click(object sender, EventArgs e)
+        {
+            Calculated();
+        }
+        /// <summary>
+        /// Calculo de la formula
+        /// </summary>
+        /// <returns></returns>
+        public object Calculated()
+        {
+            Mejor = Convert.ToInt32(txtMejor.Text);
+            Peor = Convert.ToInt32(txtPeor.Text);
+            Probable = Convert.ToInt32(txtMp.Text);
+            Total = ((Mejor + Peor + 4) * Probable) / 6;
+            txtTotal.AppendText(Convert.ToString(Total));
+
+
+
+            return Total;
+
+
+        }
+    }
+}
